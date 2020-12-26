15b02893a05b915bd247ffc847aa163f8d44dad8
Yoni Segev <Yonisegev6@gmail.com>
2020-12-26 19:28:16 +0200

    add about page

--- pages/About.jsx
+++ pages/About.jsx
@@ -2,8 +2,29 @@
 
 export function About() {
     return (
-        <div>
-            <h1>Hello From About</h1>
-        </div>
+        <section className="about-page">
+            <div className="team-members">
+
+                <h2>Meet the team</h2>
+                <div className="team-member">
+                    <div className="member-info">
+                        <h4>Reem Ben-David</h4>
+                        <img src="../assets/img/team/reem.jpeg" alt="" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero est pariatur similique iusto molestias harum numquam ex nobis a quis!</p>
+                    </div>
+                </div>
+
+                <div className="team-member">
+                    <div className="member-info">
+                        <h4>Yehonathan Segev</h4>
+                        <img src="../assets/img/team/yoni.png" alt="" />
+                        <p>An aspiring full stack developer, can't let a day pass without learning new things. <br /> Currently enrolled in the Coding Academy Bootcamp
+                    <br />
+                    23 years old from Ashdod
+                    </p>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
